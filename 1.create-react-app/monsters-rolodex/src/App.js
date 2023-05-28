02f9d429fbabc34d6916f45141168c9ae0773f43
Raghav <raghav.buzzz@gmail.com>
2023-05-29 02:37:05 +0530

    State Added

---
+++ 1.create-react-app/monsters-rolodex/src/App.js
@@ -0,0 +1,37 @@
+import { Component} from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Linda'
+        },
+        {
+          name: 'Frank'
+        },
+        {
+          name: 'Jacky'
+        }
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return <h1>{monster.name}</h1>;
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
