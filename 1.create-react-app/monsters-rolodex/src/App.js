bac8a1d48e27fe2972c14406e2a50f949ada8293
Raghav <raghav.buzzz@gmail.com>
2023-05-30 03:19:02 +0530

    Lifecycle => OnMount

--- 1.create-react-app/monsters-rolodex/src/App.js
+++ 1.create-react-app/monsters-rolodex/src/App.js
@@ -5,29 +5,38 @@ import './App.css';
 
 class App extends Component {
 
-  constructor() {
+  constructor() {    
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Linda'
-        },
-        {
-          name: 'Frank'
-        },
-        {
-          name: 'Jacky'
-        }
-      ]
+      monsters: []
     };
+    console.log('constructor');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  
   }
 
   render() {
+    console.log('render');
     return (
       <div className="App">
+        <input type="search" className='search-box' placeholder='search monsters'
+         onChange={(event) => {
+          this.setState(
+            { 
+              monsters: this.state.monsters.filter(monster => monster.name.toLowerCase().includes(event.target.value.toLowerCase()))
+            });
+         }} />
         {this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>;
+          return <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>;
         })}
       </div>
     );
